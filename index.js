4c778c76bb0783b19f275b8b558370389cb7a898
callistox9 <codecallisto@gmail.com>
2023-09-23 16:42:13 +0530

    DoneRight

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*import React from "react"
 ReactDom.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
 ReactDom.render(<ul>
     <li>name</li>
@@ -21,4 +22,55 @@ ReactDom.render(<ul>
 </ul>
 
  </nav>)
- ReactDom.render(navigation,getElementById("root"));
+ document.getElementById("root").append(JSON.stringify(navigation)); 
+ */
+
+/*import React from "react"
+import ReactDOM from "react-dom"
+function TemporaryFunction(){
+
+return(
+
+
+
+    <div>
+        <img src="./react_logo.png" width="40px"/>
+        <h1>Fun facts about react</h1>
+        <ul>
+            <li>For building complex websites</li>
+            <li>It is fun to work with</li>
+            <l1>I love JavaScript</l1>
+        </ul>
+    </div>
+)
+}
+
+ReactDOM.render(TemporaryFunction, document.getElementById("root"));
+*/
+
+
+function page()
+{
+
+return(
+<div>
+    <header>
+<nav>
+    <img scr="./react_logo.png" width="40px"></img>
+    </nav>
+    </header>
+    <h1>I am Excited to learn React</h1>
+    <ol>
+        <li>I love the Way you lie</li>
+        <li>I will be a better Person</li>
+        <li>What do you mean</li>
+        <footer>
+           <small> Developed By Soumya SOurav Guha
+            All rights Reserved</small>
+        </footer>
+    </ol>
+    </div>
+)
+}
+ReactDOM.render(page, document.getElementById(root));
+
