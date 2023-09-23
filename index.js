71724fe4bf57a0b448f4b4d9397e7b46e0021aae
callistox9 <codecallisto@gmail.com>
2023-09-23 10:48:54 +0530

    Dom done right

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ ReactDom.render(<ul>
     <li>Age</li>
     
 </ul>, getElementById("root"));
+ const page=(
+    <div>
+        <h1 className="hello"
+>Hello</h1><bl></bl>
+    <p>what do you mean when you nod your head yes but you wanna say no what do you mean?</p></div>
+ )
+ ReactDom.render(page,document.getElementById("root"));
+
+ const navigation=(<nav>
+<h1>Rockstar Shop</h1>
+<ul>
+<li>Pricing</li>
+<li>About</li>
+<li>Contact</li>
+</ul>
+
+ </nav>)
+ ReactDom.render(navigation,getElementById("root"));
